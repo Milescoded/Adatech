f4a2329122eb38b544c061ceabb354e2de6f8176
benaiah-ke <benaiahpraise@gmail.com>
2023-04-27 01:36:23 +0300

    styling changes initial

--- src/Home.js
+++ src/Home.js
@@ -11,63 +11,66 @@ import './style.css';
 
 function Home(){
     return(
-        <>
-        <div className="landingbar">
-        <div className="logo"><Link to="/Home" className="Nav1">LogoHome</Link></div>
-            <ul>
-                    <li><Link to="/Overview" className="Nav2">Overview</Link>
-                    </li>
-                    <li>
-                    <Link to="/Keytakeaways" className="Nav3">Key Takeaways</Link>
-                    </li>
-                    <li>
-                    <Link to="/Speakers" className="Nav4">Speakers</Link>
-                    </li>
-                    <li>
-                    <Link to="/Schedule" className="Nav5">Schedule</Link>
-                    </li>
-                    <li>
-                    <Link to="/Sponsors" className="Nav6">Sponsors</Link>
-                    </li>
-                    <li>
-                    <Link to="/Attend" className="Nav7">Attend</Link>
-                    </li>
-            </ul>
-        </div>
-        <div className="Landmain container">
-            <h3 className="Land1">Future of Work Summit Africa 2023 </h3>
-            <h3 className="Land2">
-                The Future of Work Summit Africa 2023 will bring together industry leaders,
-                policymakers, and experts to discuss the rapidly
-                changing world of work and its impact on Africa.<br></br>
-                June 5 - 8, 2023 - Sarit Centre, Nairobi, Kenya + Virtual
-            </h3>
-            <button className="Bt1"><span className="inBt1">Attend</span></button><button className="Bt2"><span className="inBt2">Become a Sponsor</span></button>
+        <div className="all">
+            <div className="landingbar">
+            <div className="logo"><Link to="/Home" className="Nav1">LogoHome</Link></div>
+                <ul>
+                        <li><Link to="/Overview" className="Nav2">Overview</Link>
+                        </li>
+                        <li>
+                        <Link to="/Keytakeaways" className="Nav3">Key Takeaways</Link>
+                        </li>
+                        <li>
+                        <Link to="/Speakers" className="Nav4">Speakers</Link>
+                        </li>
+                        <li>
+                        <Link to="/Schedule" className="Nav5">Schedule</Link>
+                        </li>
+                        <li>
+                        <Link to="/Sponsors" className="Nav6">Sponsors</Link>
+                        </li>
+                        <li>
+                        <Link to="/Attend" className="Nav7">Attend</Link>
+                        </li>
+                </ul>
+            </div>
+            <div className="Landmain">
+                <h3 className="Land1">Future of Work Summit Africa 2023 </h3>
+                <h3 className="Land2">
+                    The Future of Work Summit Africa 2023 will bring together industry leaders,
+                    policymakers, and experts to discuss the rapidly
+                    changing world of work and its impact on Africa.<br></br>
+                    June 5 - 8, 2023 - Sarit Centre, Nairobi, Kenya + Virtual
+                </h3>
+                <span className="Landbtns">
+                    <button className="Bt1">Attend</button>
+                    <button className="Bt2">Become a Sponsor</button>
+                </span>
+            </div>
             <div className="FooterLand">
-                <div className="ForVenue">
-                    <h4 className="Inforvenue">Venue</h4>
-                    <p className="Inforvenuep1">Sarit Centre, Nairobi, Kenya</p>
+                    <div className="ForVenue">
+                        <h4 className="Inforvenue">Venue</h4>
+                        <p className="Inforvenuep1">Sarit Centre, Nairobi, Kenya</p>
+                    </div>
+                    <div className="ForVenue1">
+                        <h4 className="Inforvenue">Date</h4>
+                        <p className-="Inforvenuep1">June 5 - 8, 2023</p>
+                    </div>
+                    <div className="ForVenue2">
+                        <h4 className="Inforvenue">Ticket</h4>
+                        <p className="Inforvenuep1" >Free - RSVP</p>
+                    </div>
                 </div>
-                <div className="ForVenue1">
-                    <h4 className="Inforvenue">Date</h4>
-                    <p className-="Inforvenuep1">June 5 - 8, 2023</p>
-                </div>
-                <div className="ForVenue2">
-                    <h4 className="Inforvenue">Ticket</h4>
-                    <p className="Inforvenuep1" >Free - RSVP</p>
-                </div>
+
+            <div>
+                
+                <OverView/>
+                <KeyTakeAways/>
+                <IndustrySponsors/>
+                <EventSpeakers/>
+                <EventSchedule/>
             </div>
         </div>
-
-        <div className="container">
-            
-            <OverView/>
-            <KeyTakeAways/>
-            <IndustrySponsors/>
-            <EventSpeakers/>
-            <EventSchedule/>
-        </div>
-        </>
 
     );
 };
--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -4,24 +4,27 @@ import "./Home.css";
 
 function OverView() {
     return(
-        <Element name="OverPage">
+        <div className="OverPage">
             <>
                 <div className="Overcs">
-                    <h4 className="Overh4">Overvview</h4>
-                    <h3 className="Overh3">
-                    Unlocking Africa's Potential: Redefining Work in a Rapidly Changing World
-                    </h3>
-                    <p className="Overp">
-                    With a focus on unlocking Africa's potential, the summit delved into topics such as digital transformation, automation, remote work, and skills development. Attendees will gain insights on the latest trends and innovations shaping the future of work in Africa, and explored strategies for harnessing these changes to drive economic growth, enhance productivity, and create more inclusive and equitable work environments. Through engaging panel discussions, interactive workshops, and networking opportunities, the summit will provide a platform for stakeholders to collaborate, learn, and shape the future of work in Africa
-                    </p>
-                    <div className="Top">
-                        <div className="a">
-                            <div className="Overpicture"></div>
-                            <div className="Overpicture"></div>
-                        </div>
-                        <div className="b">
+                    <div className="OvercsTxt">
+                        <h4 className="Overh4">Overview</h4>
+                        <h3 className="Overh3">
+                        Unlocking Africa's Potential: Redefining Work in a Rapidly Changing World
+                        </h3>
+                        <p className="Overp">
+                        With a focus on unlocking Africa's potential, the summit delved into topics such as digital transformation, automation, remote work, and skills development. Attendees will gain insights on the latest trends and innovations shaping the future of work in Africa, and explored strategies for harnessing these changes to drive economic growth, enhance productivity, and create more inclusive and equitable work environments. Through engaging panel discussions, interactive workshops, and networking opportunities, the summit will provide a platform for stakeholders to collaborate, learn, and shape the future of work in Africa
+                        </p>
+                    </div>
+                    <div className="OvercsPics">
+                        <div className="OvercsPics1">
+                            <div className="Overpicture1">
+                            </div>
                             <div className="Overpicture2"></div>
+                        </div>
+                        <div className="OvercsPics2">
                             <div className="Overpicture3"></div>
+                            <div className="Overpicture4"></div>
                     </div>
                 </div>
                 
@@ -30,7 +33,7 @@ function OverView() {
             
 
             </>
-        </Element>
+        </div>
     )
 }
 
